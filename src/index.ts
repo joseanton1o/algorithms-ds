bf3e495c352e84998d348bef3680ef9f99000ac6
joseanton1o <joseantonioperezlara@gmail.com>
2023-08-18 12:28:28 +0200

    Added exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export const helloWorld = () => console.log("Hello World!");
+// Export AVL, BST, BinTree, treeNode, Mergesort
+export { AVL } from "./DataStructures/Non-Linear/Trees/AVL";
+export { BST } from "./DataStructures/Non-Linear/Trees/BST";
+export { BinTree } from "./DataStructures/Non-Linear/Trees/BinTree";
+export { treeNode } from "./DataStructures/Non-Linear/Trees/treeNode";
+export { mergeSort } from "./Algorithms/MergeSort";
